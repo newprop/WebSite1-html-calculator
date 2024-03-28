cfd25f0b318e00955bfb0acbfc7bfc9ae1227c81
ujamir127@gmail.com <ujamir127@gmail.com>
2024-03-28 14:41:47 +0600

    Add project files.

---
+++ JavaScript.js
@@ -0,0 +1,17 @@
+﻿self.onmessage = function (e) {
+    var data = e.data;
+    var result = 0;
+
+    if (data.Operation === 'add') {
+        result = data.Number1 + data.Number2;
+    } else if (data.Operation === 'sub') {
+        result = data.Number1 - data.Number2; // Fix subtraction
+    } else if (data.Operation === 'mul') {
+        result = data.Number1 * data.Number2;
+    } else if (data.Operation === 'div') {
+        result = data.Number1 / data.Number2;
+    }
+
+    // Post the result back to the main thread
+    postMessage(result);
+};
